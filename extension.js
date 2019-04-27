7e287510578a86a17c9104d9b0c76420b4fd5bce
evilch3n <34461503+evilch3n@users.noreply.github.com>
2019-04-27 23:51:48 +0200

    resolve name conflicts

--- extension.js
+++ extension.js
@@ -3,49 +3,49 @@ const WindowAttentionHandler = imports.ui.windowAttentionHandler;
 const Lang = imports.lang;
 
 function bringToFront() {
-	this._init();
+  this._init();
 }
 
 function removeNotification() {
-	this._init();
+  this._init();
 }
-//-----------------------------------------------------------------------------
+
 bringToFront.prototype = {
-	_init : function() {
-		this._handlerid = global.display.connect('window-demands-attention', Lang.bind(this, this._onWindowDemandsAttention));
-	},
+  _init : function() {
+    this._handlerid = global.display.connect('window-demands-attention', Lang.bind(this, this._onWindowDemandsAttention));
+  },
 
-	_onWindowDemandsAttention: function(display, window) {
-		Main.activateWindow(window);
-	},
+  _onWindowDemandsAttention: function(display, window) {
+    Main.activateWindow(window);
+  },
 
-	destroy: function () {
-		global.display.disconnect(this._handlerid);
-	}
+  destroy: function () {
+    global.display.disconnect(this._handlerid);
+  }
 }
-//-----------------------------------------------------------------------------
+
 removeNotification.prototype = {
-	_init : function() {
-		global.display.disconnect(Main.windowAttentionHandler._windowDemandsAttentionId);
-	},
+  _init : function() {
+    global.display.disconnect(Main.windowAttentionHandler._windowDemandsAttentionId);
+  },
 
-	destroy: function () {
-		global.display.disconnect(this._handlerid);
-	}
+  destroy: function () {
+    global.display.disconnect(this._handlerid);
+  }
 }
-//-----------------------------------------------------------------------------
-let removeNotification;
-let bringToFront;
+
+let notificationRemover;
+let _bringToFront;
 
 function init() {}
 
-//gnome-extensions enable/disable
+
 function enable() {
-	bringToFront = new bringToFront();
-	removeNotification = new removeNotification();
+  _bringToFront = new bringToFront();
+  notificationRemover = new removeNotification();
 }
 
 function disable() {
-	bringToFront.destroy();
-	removeNotification.destroy();
+  _bringToFront.destroy();
+  notificationRemover.destroy();
 }
